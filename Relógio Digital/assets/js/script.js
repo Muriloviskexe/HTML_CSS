6939b1df1016112e2a0eca9f6bce4ce5b7925424
Muriloviskexe <muriloviskgarciacosta@gmail.com>
2023-06-13 23:17:41 -0300

    .

--- Relógio Digital/assets/js/script.js
+++ Relógio Digital/assets/js/script.js
@@ -4,12 +4,11 @@ const segundos = document.getElementById('segundos');
 
 const relogio = setInterval(function time() {
   let dateToday = new Date();
-  let hr = dateToday.getHours();  
-  let min = dateToday.getMinutes();  
-  let s = dateToday.getSeconds();  
+  let hr = dateToday.getUTCHours();  
+  let min = dateToday.getUTCMinutes();  
+  let s = dateToday.getUTCSeconds();  
 
     if (hr < 10) hr = '0' + hr;
-    if (min < 10) min = '0' + min;  
     if (min < 10) min = '0' + min;
     horas.textContent = hr;
     minutos.textContent = min;
