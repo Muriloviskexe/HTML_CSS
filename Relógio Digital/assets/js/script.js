1b6cb2f42a6c41b8c8caad5a389e77117b2dae44
Muriloviskexe <murilogccontato.com>
2023-06-20 22:19:25 -0300

    .

--- Relógio Digital/assets/js/script.js
+++ Relógio Digital/assets/js/script.js
@@ -4,9 +4,9 @@ const segundos = document.getElementById('segundos');
 
 const relogio = setInterval(function time() {
   let dateToday = new Date();
-  let hr = dateToday.getUTCHours();  
-  let min = dateToday.getUTCMinutes();  
-  let s = dateToday.getUTCSeconds();  
+  let hr = dateToday.getHours();  
+  let min = dateToday.getMinutes();  
+  let s = dateToday.getSeconds();  
 
     if (hr < 10) hr = '0' + hr;
     if (min < 10) min = '0' + min;
